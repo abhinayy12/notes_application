f51732e31b900ae91a78b9da084c6099e0476be8
Abhinay Mesipogu <mabhinay136@gmail.com>
2025-09-16 22:04:02 +0530

    Update api.ts, next.config and add backend api and vercel.json

--- frontend/notes-saas/app/utils/api.ts
+++ frontend/notes-saas/app/utils/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000',
+  baseURL: 'https://notes-application-flame-nu.vercel.app',
 });
 
 export default api;
